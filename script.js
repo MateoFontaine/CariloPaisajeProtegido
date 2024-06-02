c0314036682885796530ea8ca8406c96f133f41b
Mateo <mateofontaine7@gmail.com>
2024-06-02 12:21:54 -0300

    Resolvemos bugs dentro del menu hamburgesa

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const navLinks = document.querySelectorAll(".nav-list li a");
 
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
-})
+});
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
-})
+});
+
+// Añadir evento de clic a cada enlace de navegación
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("visible");
+    });
+});
